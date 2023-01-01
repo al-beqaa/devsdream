76d1ffef0d0600837ae0b30459bb78a9c4d5b863
ChromaKey <williamcipher2006@gmail.com>
2023-01-01 01:16:35 -0500

    Refactor examplemod

--- src/main/java/net/beqaa/devsdream/DevelopersDream.java
+++ src/main/java/net/beqaa/devsdream/DevelopersDream.java
@@ -1,14 +1,17 @@
-package net.fabricmc.example;
+package net.beqaa.devsdream;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class DevelopersDream implements ModInitializer {
+
+	public static final String MODID = "devsdream";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	@Override
 	public void onInitialize() {
